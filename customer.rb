c0db27c602d91f63b3b74fa886b0c83211921e01
Graeme Brown <graemehbrown@googlemail.com>
2017-11-29 15:09:54 +0000

    Added buy_drink_from_pub to Customer class.

--- customer.rb
+++ customer.rb
@@ -8,7 +8,22 @@ class Customer
   end
 
   def take_money_from_wallet(value)
-    return @wallet -= value
+     return @wallet -= value
   end
+
+  def buy_drink_from_pub(pub, drink_name)
+    price = find_drink_price(pub, drink_name)
+    take_money_from_wallet(price)
+    pub.add_money_to_till(price)
+  end
+
+  def find_drink_price(pub, drink_name)
+    for drink in pub.drinks()
+      if pub.pub_has_drink?(drink_name)
+        return drink.price()
+      end
+    end
+  end
+
 
 end
--- spec/customer_spec.rb
+++ spec/customer_spec.rb
@@ -1,11 +1,18 @@
 require('minitest/autorun')
 require('minitest/rg')
 require_relative('../customer')
+require_relative('../pub')
+require_relative('../drinks')
 
 class TestCustomer < MiniTest::Test
 
   def setup
     @first_customer = Customer.new('Fred', 20.00)
+    @first_drink = Drink.new('Guiness', 3.50)
+    @second_drink = Drink.new('Gin and Tonic', 4.50)
+    @third_drink = Drink.new('Red Wine', 3.00)
+    @drinks = [@first_drink, @second_drink, @third_drink]
+    @pub = Pub.new('The Red Lion', 50.00, @drinks)
   end
 
   def test_get_customer_name()
@@ -24,7 +31,9 @@ class TestCustomer < MiniTest::Test
   end
 
   def test_duy_drink_from_pub
-    
+    @first_customer.buy_drink_from_pub(@pub, @first_drink.name())
+    assert_equal(16.50, @first_customer.wallet())
+    assert_equal(53.50, @pub.till())
   end
 
 
